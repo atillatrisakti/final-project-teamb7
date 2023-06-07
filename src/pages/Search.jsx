b5b01158b08b2386132ba430093ee7bd0e487184
Atilla Silva T <111876471+atillatrisakti@users.noreply.github.com>
2023-06-07 11:08:15 +0700

    adding navbar (#1)
    Co-authored-by: atilla trisakti <atillatrisakti01@gmail.com>

---
+++ src/pages/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+function Search() {
+  return (
+    <Container className="mt-5">
+      <h4 className="fw-700">Pilih Penerbangan</h4>
+      <Container>
+        <Row>
+          <Col></Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Search;
