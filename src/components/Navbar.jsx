3e13128983d5a0c852c674019b2bc0431ee8b904
syuhda <syuhdafakhrunnisa19@gmail.com>
2023-06-20 18:44:44 +0700

    banner and home design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Container, Navbar, Form, Nav } from "react-bootstrap";
-import logo from "../assets/navbar/logo.svg";
+// import logo from "../assets/navbar/logo.svg";
+import logo from "../assets/navbar/logo-name.svg";
+
 import { Link } from "react-router-dom";
 import login from "../assets/navbar/fi_log-in.svg";
 import "../styles/Navbar.css";
 
 function Navbarr() {
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className="py-0 mb-0">
       <Container>
         <Navbar.Brand href="/" className="text-dark">
-          <img src={logo} alt="logo" fluid width="80" height="80" />
+          <img src={logo} alt="logo" fluid width="100" height="70" />
         </Navbar.Brand>
         <Form className="d-flex w-50">
-          <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
         </Form>
         <Nav className="">
           <Link to={"/login"} style={{ textDecoration: "none" }}>
